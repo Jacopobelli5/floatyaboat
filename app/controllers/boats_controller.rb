9ab09ce5a7250febe0a5a061ad60dc35ff5e30b3
jacopo belli <jacopo.b2016@gmail.com>
2022-08-01 19:26:18 +0100

    controller boat, views boats, routes, seed done

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,33 @@
 class BoatsController < ApplicationController
+  def index
+    @boats = Boat.all
+  end
+
+  def show
+    @boat = Boat.find(params[:id])
+  end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    if @boat.save
+      redirect_to boat_path(@boat)
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @boat.destroy
+    redirect_to boats_path, status: :see_other
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name, :type, :address, :price, :description)
+  end
 end
