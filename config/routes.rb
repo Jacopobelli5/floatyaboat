f35a0ae297a1a66512c8c3d75ba37be95e4bef14
huda salameh <huda.salameh44@gmail.com>
2022-08-02 22:00:22 +0100

    add booking routes, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :boats
+  resources :boats do
+    resources :bookings, only: [:new, :create, :edit]
+  end
+  resources :bookings, only: [:show] # localhost:3000/bookings/1/show
 end
