b27c1551616d5f54feb4421fb1c418204e5c49ad
Sarhan Alim <sarhan.alim@gmail.com>
2022-08-03 16:44:03 +0100

    rename 'type' column in boats table due to Active Record errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_01_213925) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_03_153244) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "boats", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "boat_type"
     t.string "address"
     t.integer "price"
     t.text "description"
