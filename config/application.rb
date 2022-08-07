42753b29775a6cbc2dfd73f728e74f1b7fdd44b2
Sarhan Alim <sarhan.alim@gmail.com>
2022-08-07 22:35:06 +0100

    roll back some temp changed made to fix an scss issue

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module Floatyaboat
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.assets.paths << Rails.root.join('vendor', 'assets')
   end
 end
